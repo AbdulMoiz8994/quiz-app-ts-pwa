fdc88641a42e29dc8a8e55f8a6cbf1be40c5029f
AbdulMoiz8994 <moiza8994@gmail.com>
2021-05-13 23:15:39 +0500

    we make serviceWokrer file toad dthe api and file

---
+++ public/serviceWorker.js
@@ -0,0 +1,45 @@
+
+const cacheName="Quiz-Web-App"
+
+
+const addFile=[
+    '/',
+    'static/js/bundle.js',
+    '/static/js/main.chunk.js',
+    '/static/js/vendors~main.chunk.js',
+    '/favicon.js',
+    '/index.html',
+    '/logo192.png',
+    '/logo512.png',
+    '/manifest.json',
+    '/serviceWorker.js',
+    "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple"
+];
+
+this.addEventListener('activate',() =>{
+    console.log("The file has been activated");
+})
+
+
+this.addEventListener('install', (event) =>{
+   console.log("The Service Wokrer has installed");
+   event.waitUntil(
+   caches.open(cacheName).then((data) =>{
+       return data.addAll(addFile)
+           
+   }).catch((err) =>{
+     console.log("Error" , err);
+   })  
+   )
+})
+
+this.addEventListener('fetch',(event) =>{
+    console.log("Fetch Data", event.request);
+    event.respondWith(
+    caches.match(event.request).then((response) =>{
+       return response || fetch(event.request)
+    }).catch((err) =>{
+         console.log("Error In Fethcing Data", err);
+    })
+    )
+})
